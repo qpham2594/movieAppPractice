e6ebc09bcca954288743fef2588c9a272d6fa6c8
Quynh <qpham2594@gmail.com>
2023-07-23 14:53:29 -0400

    update

--- homepage.js
+++ homepage.js
@@ -1,7 +1,71 @@
 const userSet = document.querySelector(".userSet");
 const myButton = document.getElementById("myButton");
-
+const movieAPI = document.querySelector('.movieList')
+const numberOfUser = document.getElementsByClassName('numberOfUser')
+const generateBtn = document.querySelector('.generatebtn')
 const { classList: dropDownMenuClassList} = document.getElementById("dropDownMenu")
+
+
+generateBtn.addEventListener('click',displayMovies)
+
+const movieAPIKey = 'eb27c13c3073bd1a4e7f1f8f94714eaf'
+
+async function getMovies(){
+    try {
+        const res = await fetch (`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${movieAPIKey}`)
+        const movieData = await res.json()
+        return movieData.results
+    } catch (error) {
+        console.error ('Unable to get data', error)
+        return []
+    }
+   
+}
+
+const randomizeMovies = (array) => {
+ return array.slice().sort(() => Math.random() - 0.5);
+}
+
+async function shuffleMovies() {
+  const movieList = await getMovies();
+  return randomizeMovies(movieList).slice(0,4)
+}
+
+async function displayMovies() {
+  try {
+  const movieListDiv = document.querySelector('.movieList');
+ 
+
+  const movies = await shuffleMovies()
+  console.log(movies)
+
+  shuffleMovies().then((movies) => {
+    movies.forEach((movie) => {
+      const {poster_path,title,release_date,overview} = movie
+
+      const moviePoster = document.createElement('img')
+      const posterURL = `https://image.tmdb.org/t/p/w500${poster_path}`
+      moviePoster.src = posterURL
+      movieListDiv.appendChild(moviePoster)
+
+
+      const movieTitle = document.createElement('h3')
+      movieTitle.textContent = title
+      movieListDiv.appendChild(movieTitle)
+  
+      const movieReleaseDate = document.createElement('p')
+      movieReleaseDate.textContent = release_date
+      movieListDiv.appendChild(movieReleaseDate)
+  
+      const movieOverview = document.createElement('p')
+      movieOverview.textContent = overview
+      movieListDiv.appendChild(movieOverview)  
+    })
+  })
+} catch (error) {
+  console.error ("unable to get data", error)
+}
+}
 
 
 const toggleDropDown = () => dropDownMenuClassList.toggle("show");
@@ -9,15 +73,15 @@ myButton.addEventListener("click", toggleDropDown);
 
 const showButtons = num => {
    
-    userSet.innerHTML = " ";
+  userSet.innerHTML = ""; // Clear any previously displayed buttons
 
-    for (let i = 1; i <= num; i++) {
-        const userButton = document.createElement("a")
-        userButton.classList.add("userButton")
-        userButton.textContent = `User ${i}`;
-        userButton.href = `voting.html`;
-        userSet.appendChild(userButton)
-    }
+  for (let i = 1; i <= num; i++) {
+    const userButton = document.createElement("a");
+    userButton.classList.add("userButton");
+    userButton.textContent = `User ${i}`;
+    userButton.href = `voting.html?user=${i}`;
+    userSet.appendChild(userButton);
+  }
 }
 
 const dropdowns = document.getElementsByClassName("dropdown-content");
--- voting.js
+++ voting.js
@@ -1,13 +1,9 @@
-const movieAPI = document.getElementById('movieList')
-const numberOfUser = document.getElementsByClassName('numberOfUser')
 
 
-/*
+const votingButtons = document.getElementsByClassName('votingbutton');
 
-const movieAPIKey = 'eb27c13c3073bd1a4e7f1f8f94714eaf'
-try {
-    const res = await fetch (`https://www.themoviedb.org/settings/${movieAPIKey}${userInput}`)
-    const movieData = await res.json()
-} catch {error} {
-    console.error ('Unable to get data', error)
-}*/
+for (let i = 0; i < votingButtons.length; i++) {
+  votingButtons[i].addEventListener("click", function() {
+    window.location.href = `homepage.html`;
+  });
+}
